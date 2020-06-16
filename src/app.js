32440e04d29144dea4c424ea2a632b79b8b5ab98
sakthivel9963 <sakthivel9963@gmail.com>
2020-06-16 23:47:01 +0530

    default port added

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const path = require('path');
+const favicon = require('serve-favicon');
 const router = require('./router');
 const { notFound, errorHandler } = require('./middleware/utils');
 require('dotenv').config();
@@ -11,6 +12,7 @@ const winston = require('./middleware/winston');
 const app = express();
 
 app.use('/', express.static(path.join(__dirname, '../public')));
+app.use(favicon(path.join(__dirname, '../public/assets/images/favicon.ico')));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-const { PORT } = process.env;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`app listening on url : http://localhost:${PORT}`);
