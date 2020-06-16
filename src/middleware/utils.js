b64d7a3e417b3d717e4484a5ae96feebe3a5b96c
sakthivel9963 <sakthivel9963@gmail.com>
2020-06-16 23:59:35 +0530

    log

--- src/middleware/utils.js
+++ src/middleware/utils.js
@@ -8,7 +8,7 @@ function notFound(req, res, next) {
 
 // eslint-disable-next-line no-unused-vars
 function errorHandler(error, req, res, next) {
-  console.error(error);
+  // console.error(error);
   const { message, stack } = error;
   const status = res.statusCode === 200 ? 500 : res.statusCode;
   logger.error(`${status} - ${req.originalUrl} - ${message} - ${stack} `);
