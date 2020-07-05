0594ee55c24a598ffd7809636e906ec5e87ad522
sakthivel9963 <sakthivel9963@gmail.com>
2020-07-05 18:19:14 +0530

    :tada: change the basic routing and added ping in the console

--- src/index.js
+++ src/index.js
@@ -4,4 +4,5 @@ const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`app listening on url : http://localhost:${PORT}`);
+  console.log(`http://localhost:${PORT}/api/v1/ping`);
 });
